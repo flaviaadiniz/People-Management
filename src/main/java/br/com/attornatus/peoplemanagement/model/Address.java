0616db788f760dbfc224cad7744b1c5edd8bcdb5
flaviaadiniz <flaviaadiniz@gmail.com>
2023-02-22 09:56:57 -0300

    Adjusted Lombok annotations

--- src/main/java/br/com/attornatus/peoplemanagement/model/Address.java
+++ src/main/java/br/com/attornatus/peoplemanagement/model/Address.java
@@ -2,19 +2,17 @@ package br.com.attornatus.peoplemanagement.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 @Table(name = "addresses")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JsonBackReference
     private Person person;
 
@@ -23,62 +21,5 @@ public class Address {
     private Integer number;
     private String city;
     private boolean main;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public void setPerson(Person person) {
-        this.person = person;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public boolean isMain() {
-        return main;
-    }
-
-    public void setMain(boolean main) {
-        this.main = main;
-    }
 
 }
--- src/main/java/br/com/attornatus/peoplemanagement/model/Person.java
+++ src/main/java/br/com/attornatus/peoplemanagement/model/Person.java
@@ -3,17 +3,12 @@ package br.com.attornatus.peoplemanagement.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
+import lombok.Data;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
-
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 @Table(name = "people")
 public class Person {
 
@@ -28,48 +23,9 @@ public class Person {
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate birthDate;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "person_id")
     @JsonManagedReference
-    private List<Address> addresses = new ArrayList<>();
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public List<Address> getAddress() {
-        return addresses;
-    }
-
-    public void setAddress(Address address) {
-        Address address1 = new Address();
-        address1.setStreet(address.getStreet());
-        address1.setPostalCode(address.getPostalCode());
-        address1.setNumber(address.getNumber());
-        address1.setCity(address.getCity());
-        address1.setMain(address.isMain());
-        this.addresses.add(address1);
-    }
+    private List<Address> addresses;
 
 }
